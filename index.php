4080e476e8c1fded5cf6e52a191d47972d51f0e5
Malibu <malibu1106@gmail.com>
2024-05-07 15:40:45 +0200

    Push pour fabien page detail voyage

--- index.php
+++ index.php
@@ -1,32 +1,40 @@
 <?php
-$pathSet = false;
-$region = ''; // Initialisation de la variable $region
+$regionSet = false;
+$envieSet = false;
+$region = ''; 
 $envie = ''; 
 if(isset($_GET['region'])) {
     $region = $_GET['region'];
-    $pathSet = true;
+    $regionSet = true;
 }
 if(isset($_GET['envie'])) {
     $envie = $_GET['envie'];
-    $pathSet = true;
+    $envieSet = true;
 }
 ?>
-
-
 
 <?php include 'prebody.html'?>
 <?php include 'header.html'?>
 
-<?php if ($pathSet === true){
-
- include ("pages/$region$envie.html");
+<?php 
+if ($regionSet == true && $envieSet == true) {
+    include ("pages/$region$envie.php");
+} else if ($regionSet == true) {
+    foreach (glob("pages/*$region*.php") as $filename) {
+        if (basename($filename) !== "$region.php") {
+            include $filename;
+        }
+    }
+} else if ($envieSet == true) {
+    foreach (glob("pages/*$envie*.php") as $filename) {
+        if (basename($filename) !== "$envie.php") {
+            include $filename;
+        }
+    }
+} else {
+    include 'accueil.html';
 }
-else{
-
- include 'accueil.html';}
- 
- ?>
-
+?>
 
 <?php include 'footer.html'?>
 <?php include 'postbody.html'?>
---
+++ pages/grandestculture.php
@@ -0,0 +1,8 @@
+<div class="cut-blanc"></div>
+    <section class="voyage-details">
+        detail voyage
+    
+
+
+
+</section>
