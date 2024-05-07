cf47276f82b648e53d573c4aef5a8a1044aebba7
cy-fa <cypher_fab@proton.me>
2024-05-07 16:50:23 +0200

    html/css-detail-voyage

--- pages/grandestculture.php
+++ pages/grandestculture.php
@@ -1,7 +1,21 @@
 <div class="cut-blanc"></div>
-    <section class="voyage-details">
-        detail voyage
-    
+<section class="voyage-details">
+    detail voyage
+    <h2 class=titre-voyage-details>VOYAGE</h2>
+    <h4>Phrase pour</h4>
+    <div class="conteneur-photo">
+        <article class="voyage-article">
+            <figure>
+                <img class="photo-voyage" title="grandest" src="./img/voyages/promos/grandest.jpg" alt="grandest">
+                <figcaption class="conteneur-figcaption">
+                    <div>
+                        <h4>TITRE FIGCAPTION</h4>
+                        <p>Texte,texte,texte,texte,texte,</p>
+                    </div>
+                </figcaption>
+            </figure>
+        </article>
+    </div>
 
 
 
